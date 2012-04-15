271d8ffd135fef8a32ae80ceac355a78aed62c3e
Bill Agee <billagee@gmail.com>
2012-04-15 13:21:36 -0700

    First commit of isVistaOrNewer.cs

---
+++ isVistaOrNewer/dotnet/csharp/isVistaOrNewer.cs
@@ -0,0 +1,29 @@
+﻿// isVistaOrNewer.cs
+//
+// Checks if the OS is Vista or later and prints the result to the console.
+//
+// From https://github.com/billagee/test-polyglot
+//
+// Built with MS Visual Studio C# Express 2010
+
+using System;
+
+namespace isVistaOrNewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatingSystem osInfo = Environment.OSVersion;
+            Console.WriteLine("Major version is: " +
+                osInfo.Version.Major.ToString());
+            Console.WriteLine("Minor version is: " +
+                osInfo.Version.Minor.ToString());
+
+            if (osInfo.Version.Major >= 6)
+                Console.WriteLine("OS is Vista or newer.");
+            else
+                Console.WriteLine("OS is XP or newer.");
+        }
+    }
+}
