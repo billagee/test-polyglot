55e68079bb924ec797cc37a695375a67d111231d
Bill Agee <billagee@gmail.com>
2012-04-22 22:00:09 -0700

    First commit of printDesktopUiaElementName.ps1

---
+++ printDesktopUiaElementName/ms-ui-automation-managed-api/powershell/printDesktopUiaElementName.ps1
@@ -0,0 +1,22 @@
+# printDesktopUiaElementName.ps1
+#
+# From https://github.com/billagee/test-polyglot
+#
+# Demonstrates that when using the managed UI Automation API, the root
+# (dekstop) AutomationElement has an empty Name.
+#
+# Instead, to identify the desktop you can use its ClassName.
+#
+# Tested with:
+# - Windows 7 32-bit
+# - PowerShell 2.0
+
+# Get the root element
+$rootElement = [Windows.Automation.AutomationElement]::RootElement
+# Print its Name and ClassName
+Write-Host "Note: In the managed UIA API, the desktop name is empty:"
+Write-Host $rootElement.Current.Name
+"'" + $rootElement.Current.Name + "'"
+Write-Host "Instead, you can identify the desktop by its ClassName, #32769:"
+"'" + $rootElement.Current.ClassName + "'"
+
